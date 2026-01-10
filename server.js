b3ebf0c77661894ea5c1d9efceb6936ec4a1caff
annilla2 <anilanuredini92@gmail.com>
2026-01-09 16:08:19 -0800

    ndryshime ne server

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 import express from "express";
 import cors from "cors";
 import productRoute from "./routes/product.js"; // pa "s"
+import { swaggerUi, specs } from "./swagger.js"; // <--- shtim për swagger
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+// Routes ekzistuese
 app.use("/products", productRoute);
 
 const PORT = 5000;
