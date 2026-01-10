9220e808d86cd963bb8d990768455af4753eaac6
annilla2 <anilanuredini92@gmail.com>
2026-01-10 10:30:46 -0800

    permiresimi

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Auth Service running on port ${PORT}`);
 });
